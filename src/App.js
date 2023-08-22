51e31d7a90f91c4ca051a45960ebca63254c7875
Tan Hao Xiang <Tp060906@mail.apu.edu.my>
2023-08-23 01:06:10 +0800

    Initialize front end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
+import Main from './pages/main';
+import MyJournal from './pages/myjournal';
+import UserProfile from './pages/userprofile';
+import Journal from './pages/journal';
+
+
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-gray-500 underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/myjournal" element={<MyJournal />} />
+        <Route path="/userprofile" element={<UserProfile />} />
+        <Route path="/journal" element={<Journal />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/myjournal.js
@@ -0,0 +1,8 @@
+// import './App.css';
+// import backgroundImg from './bg.png'
+
+export default function myjournal(){
+    return(
+        <p className="text-gray-500 text-3xl py-6"> "Testing" </p>
+    );
+}
